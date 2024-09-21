c4f8cdf79c0087445100e6f84a8306b0f9a3332b
NicolasMontone <montonenicolas01@gmail.com>
2024-09-21 15:55:34 -0300

    Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,7 @@ export default async function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <main className="min-h-screen flex flex-col items-center max-w-full mx-auto">
+          <main className="min-h-screen overflow-hidden flex flex-col items-center max-w-full mx-auto">
             <div className="h-screen w-full dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center">
               <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
 
