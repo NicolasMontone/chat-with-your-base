b69d022a7d2d611380b3925f13235463a9a08d9c
NicolasMontone <montonenicolas01@gmail.com>
2024-09-21 14:30:19 -0300

    Add loading state in tool calling.

--- components/chat.tsx
+++ components/chat.tsx
@@ -9,6 +9,15 @@ import TextSkeleton from './text-skeleton'
 import Markdown from 'react-markdown'
 import CodeBlock from './code-block'
 
+const toolCallToNameText = {
+  getExplainForQuery: 'Getting query plan...',
+  getForeignKeyConstraints: 'Fetching foreign key relationships...',
+  getIndexes: 'Listing table indexes...',
+  getIndexStatsUsage: 'Analyzing index usage...',
+  getPublicTablesWithColumns: 'Retrieving public tables and columns...',
+  getTableStats: 'Collecting table statistics...',
+}
+
 export default function Chat() {
   const messagesChat = useRef<HTMLDivElement | null>(null)
 
@@ -37,79 +46,112 @@ export default function Chat() {
     return isLoading && lastMessageIsUser
   }, [isLoading, messages])
 
+  const toolsLoading = useMemo(() => {
+    const toolInvocation = messages[messages.length - 1]?.toolInvocations
+
+    return (toolInvocation ?? []).filter((tool) => tool.state === 'call')
+  }, [isLoading, messages])
+
   return (
     <div
       ref={messagesChat}
       className="h-screen overflow-auto sm:min-w-[70%] sm:w-[1000px] sm:max-w-[840px] relative sm:p-28 sm:pt-0 p-12 pt-0 min-w-[90%]"
     >
-      {messages?.map((m: Message) => (
-        <div key={m.id}>
-          {m.role === 'user' ? (
-            <div className="text-2xl text-primary font-semibold">
+      {messages?.map((m: Message) => {
+        if (m.toolInvocations) return null
+
+        return (
+          <div key={m.id}>
+            {m.role === 'user' ? (
+              <div className="text-2xl text-primary font-semibold">
+                <motion.span
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 0.5 }}
+                >
+                  {m.content}
+                </motion.span>
+              </div>
+            ) : (
               <motion.span
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
-                transition={{ duration: 0.5 }}
+                transition={{ duration: 0.75 }}
+                className="mb-2 text-primary"
               >
-                {m.content}
+                <Markdown
+                  components={{
+                    code: ({ children, className }) => {
+                      return (
+                        <CodeBlock
+                          language={
+                            className?.includes('sql') ? 'sql' : 'markup'
+                          }
+                        >
+                          {children}
+                        </CodeBlock>
+                      )
+                    },
+                    li: ({ children }) => <li className="my-1">{children}</li>,
+                    ul: ({ children }) => (
+                      <ul className="list-disc pl-4 my-1">{children}</ul>
+                    ),
+                    h1: ({ children }) => (
+                      <h1 className="text-2xl font-bold my-2">{children}</h1>
+                    ),
+                    h2: ({ children }) => (
+                      <h2 className="text-xl font-semibold my-1">{children}</h2>
+                    ),
+                    h3: ({ children }) => (
+                      <h3 className="text-lg font-medium my-1">{children}</h3>
+                    ),
+                    h4: ({ children }) => (
+                      <h4 className="text-base font-normal my-1">{children}</h4>
+                    ),
+                    h5: ({ children }) => (
+                      <h5 className="text-sm font-normal my-1">{children}</h5>
+                    ),
+                    h6: ({ children }) => (
+                      <h6 className="text-xs font-normal my-1">{children}</h6>
+                    ),
+                  }}
+                >
+                  {m.content}
+                </Markdown>
               </motion.span>
-            </div>
-          ) : (
-            <motion.span
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ duration: 0.75 }}
-              className="mb-2 text-primary"
+            )}
+            <br />
+            {m.role === 'assistant' && !m.toolInvocations && (
+              <hr className="my-4" />
+            )}
+          </div>
+        )
+      })}
+      {(showSkeleton || toolsLoading.length > 0) && <TextSkeleton />}
+      {toolsLoading.map((tool) => {
+        const aiRunningText =
+          toolCallToNameText[
+            tool.toolName as keyof typeof toolCallToNameText
+          ] ?? ''
+
+        return (
+          aiRunningText && (
+            <motion.div
+              key={tool.toolCallId}
+              initial={{ opacity: 0.5 }}
+              animate={{ opacity: [0.5, 1, 0.5] }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                ease: 'easeInOut',
+              }}
+              className="text-primary font-semibold mt-2"
             >
-              <Markdown
-                components={{
-                  code: ({ children, className }) => {
-                    return (
-                      <CodeBlock
-                        language={className?.includes('sql') ? 'sql' : 'markup'}
-                      >
-                        {children}
-                      </CodeBlock>
-                    )
-                  },
-                  li: ({ children }) => <li className="my-1">{children}</li>,
-                  ul: ({ children }) => (
-                    <ul className="list-disc pl-4 my-1">{children}</ul>
-                  ),
-                  h1: ({ children }) => (
-                    <h1 className="text-2xl font-bold my-2">{children}</h1>
-                  ),
-                  h2: ({ children }) => (
-                    <h2 className="text-xl font-semibold my-1">{children}</h2>
-                  ),
-                  h3: ({ children }) => (
-                    <h3 className="text-lg font-medium my-1">{children}</h3>
-                  ),
-                  h4: ({ children }) => (
-                    <h4 className="text-base font-normal my-1">{children}</h4>
-                  ),
-                  h5: ({ children }) => (
-                    <h5 className="text-sm font-normal my-1">{children}</h5>
-                  ),
-                  h6: ({ children }) => (
-                    <h6 className="text-xs font-normal my-1">{children}</h6>
-                  ),
-                }}
-              >
-                {m.content}
-              </Markdown>
-            </motion.span>
-          )}
-
-          <br />
-
-          {m.role === 'assistant' && !m.toolInvocations && (
-            <hr className="my-4" />
-          )}
-        </div>
-      ))}
-      {showSkeleton && <TextSkeleton />}
-
+              <p>{aiRunningText}</p>
+            </motion.div>
+          )
+        )
+      })}
       <Form
         onChange={handleInputChange}
         value={input}
