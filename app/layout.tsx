56241a1c3efa8bdd39c8280fa06e7643b6f6f634
NicolasMontone <montonenicolas01@gmail.com>
2024-09-22 17:54:21 -0300

    refactor: Update CodeBlock component to improve code highlighting in chat

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: 'Next.js and Supabase Starter Kit',
-  description: 'The fastest way to build apps with Next.js and Supabase',
+  title: 'Chat with your database',
+  description: 'The AI that really knows your postgres DB',
 }
 
 export default async function RootLayout({
