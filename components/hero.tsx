a6e65a0322fd8c432cb30b207ff588691dccbb5e
NicolasMontone <montonenicolas01@gmail.com>
2024-09-22 13:09:36 -0300

    refactor: Update hero component to fix typo in text

--- components/hero.tsx
+++ components/hero.tsx
@@ -12,7 +12,7 @@ export function Hero() {
       />
       <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
         <h1 className="text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
-          Chat with your <br /> base
+          Chat with your <br /> database
         </h1>
         <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">
           Unlock the power of your data: Connect any Postgres database and let
