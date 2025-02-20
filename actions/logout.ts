afc018319b40a37c4b18f5dbedab5e52fa3100be
NicolasMontone <montonenicolas01@gmail.com>
2025-02-19 22:14:56 -0300

    fix logout.

--- actions/logout.ts
+++ actions/logout.ts
@@ -4,7 +4,7 @@ import { createClient } from '@/utils/supabase/server'
 import { redirect } from 'next/navigation'
 
 export const logoutAction = async () => {
-  const supabase = createClient()
+  const supabase = await createClient()
   await supabase.auth.signOut()
   return redirect('/login')
 }
